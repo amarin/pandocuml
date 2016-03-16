82447edc7dad3f4bb5fb7159c16376f4df76a8a6
Марин Алексей <amarin@MacBook-Marin.local>
2016-03-16 12:55:29 +0300

    Moved windows command into separate folder

--- makehtml.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-@SET mypath=%~dp0
-@SET filepath=%~f1
-@SET plantuml=%mypath%plantuml.py
-@SET result=%~n1.html
-@SET pandoc=%mypath%pandoc.exe
-
-%pandoc% --filter %plantuml% -o %result% %filepath%
